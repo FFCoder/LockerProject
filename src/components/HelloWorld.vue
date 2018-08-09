a695b4582b2863b55156e7864d72b8145e37ec1b
Jonathon Chambers <jonathonc.94@gmail.com>
2018-08-09 18:52:25 -0400

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <nav>
+      <h3>Locker Project</h3>
+      <div class="navLinks">
+        <ul>
+          <li>Home</li>
+          <li>Report a Repair</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+
+
